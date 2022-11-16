db2f085e29c9abd11982d961a9ce0330c6331181
elnaz famkar <ellefamkar@gmail.com>
2022-11-16 22:16:10 +0330

    fix : modify showCity codes to fix bug

--- src/app.js
+++ src/app.js
@@ -95,8 +95,8 @@ const getForcast = (city) => {
     let temperature = document.querySelector(".current-temp");
     let dateElement = document.querySelector("#weather-time");
     let iconElement = document.querySelector(".temperature-emoji");
-    farenheight.classList.remove("active");
-    celcius.classList.add("active");
+    // farenheight.classList.remove("active");
+    // celcius.classList.add("active");
     // let country = response.data.country;
     // let countryAbr = country.substring(0,3);
     // countryName.innerHTML = `(${countryAbr})`;
